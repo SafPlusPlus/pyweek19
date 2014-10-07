8dabe258ea48cf3b11741b0e47b0bd77be9fab11
SafPlusPlus <safariman@gmail.com>
2014-10-07 10:44:56 +0200

    random work on asset loader

--- pw19/__main__.py
+++ pw19/__main__.py
@@ -1,4 +1,70 @@
+import os, sys, logging, pygame
+from logging import debug, info
+from assets import Assets
+
+# nicked from http://stackoverflow.com/questions/14058453/making-python-loggers-output-all-messages-to-stdout-in-addition-to-log
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+root.addHandler(ch)
+
+class App:
+
+    def __init__(self, name=None):
+        self._running = True
+        self._display_surf = None
+        self.size = self.weight, self.height = 640, 480
+        self.name = name or 'pw19'
+        self.assets = Assets()
+        debug('main init done')
+ 
+    def on_init(self):
+        debug('on_init started')
+        pygame.init()
+
+        self.assets.load_all()
+    
+        # load icon and set it
+        # icon = pygame.image.load(os.path.join('.', 'afuckingbeachballright_icon.png'))
+        
+        
+        pygame.display.set_icon(icon)
+        pygame.display.set_caption(self.name)
+
+        self._surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._running = True
+        debug('on_init done')
+ 
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False
+
+    def on_loop(self):
+        pass
+
+    def on_render(self):
+        pass
+
+    def on_cleanup(self):
+        pygame.quit()
+ 
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+ 
+        while( self._running ):
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
 
 def main():
     """ your app starts here
     """
+    theApp = App()
+    debug('starting main app')
+    theApp.on_execute()
